977ff845680bb8457c78a4eff8c03bc2a82e4d75
Aswathi-6 <aswathimurugesan2406@gmail.com>
2026-04-17 12:03:14 +0530

    Files Modified

--- Temp.java
+++ Temp.java
@@ -1,5 +1,8 @@
 public class Temp {
     public static void main(String[] args) {
-        
+        println("Hello World");
+    }
+    public static void println(String message){
+        System.out.println("message");
     }
 }
